bf46c04b724f11c203e49dfe62ce695a248d2cb0
Adil Rashid <adilrashid090@gmail.com>
2022-01-16 00:33:18 +0530

    2nd section

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $("#owl-demo").owlCarousel({
+    navigation: true, // Show next and prev buttons
+
+    slideSpeed: 300,
+    paginationSpeed: 400,
+
+    items: 1,
+
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+  });
+});
