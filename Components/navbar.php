f08da6030148d04734028d00d4004aee20979d81
Bhagyashree Patra <70473390+bhagyashree0654@users.noreply.github.com>
2022-02-07 10:03:52 +0530

    Add files via upload

---
+++ Components/navbar.php
@@ -0,0 +1,57 @@
+<nav class="navbar sticky-top navbar-expand-lg navbar-light myNavBar">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php" style="color: white"
+            >MyCery</a
+          >
+          <button
+            class="navbar-toggler"
+            type="button"
+            style="color: white"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href="index.php"
+                  style="color: white"
+                  >Home</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="aboutUs.php" style="color: white"
+                  >About Us</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contactus.php" style="color: white"
+                  >Contact Us</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="privacy.php" style="color: white"
+                  >Terms & Privacy</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="career.php" style="color: white"
+                  >Career</a
+                >
+              </li>
+            </ul>
+            <form class="d-flex">
+              <button class="btn headerBtn" type="submit">
+                Download The App
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
